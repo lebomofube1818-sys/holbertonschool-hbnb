9dfa42de7be6a6ffbffa7413127655cd33b88931
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-03-19 11:58:33 +0200

    Add SQL schema and initial data, test CRUD operations

---
+++ part3/seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (id, first_name, last_name, email, password, is_admin)
+VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'Admin',
+    'HBnB',
+    'admin@hbnb.io',
+    '$2b$12$LznZn1Kpbqw5ZJk79EONduWBylYVbEz4sriKPCwO7O4NgrL20hPjq',
+    TRUE
+);
+
+INSERT INTO amenities (id, name) VALUES
+('36a0fd5f-f319-4887-b0aa-3e02c1109624', 'WiFi'),
+('855d0931-8cb7-4ecb-a9f2-ff93a4125ca8', 'Swimming Pool'),
+('b1948101-106c-48b9-93f2-31d70b2219d0', 'Air Conditioning');
